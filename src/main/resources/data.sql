3b9d94fbf0a716ab4d32fb5cac50a07c5d690601
issame <issameartiste@gmail.com>
2023-06-25 21:55:39 +0100

    adding controllers and some data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into currency_exchange
+(conversion_multiple,id,currency_from,currency_to,environment)
+values(55,10001,'USD','DZD','');
+insert into currency_exchange
+(conversion_multiple,id,currency_from,currency_to,environment)
+values(65,10002,'USD','EUR','');
+insert into currency_exchange
+(conversion_multiple,id,currency_from,currency_to,environment)
+values(45,10003,'EUR','DZD','');
+insert into currency_exchange
+(conversion_multiple,id,currency_from,currency_to,environment)
+values(15,10004,'EUR','DZD','');
