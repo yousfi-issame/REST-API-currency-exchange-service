e6b5fe4d9e5b85cf435586fb54762f3e08f80dce
issame <issameartiste@gmail.com>
2023-06-29 17:59:11 +0100

    messing around with resilience4j features

---
+++ src/main/java/com/MicroServices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,23 @@
+package com.MicroServices.currencyexchangeservice;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+    private Logger logger =
+            LoggerFactory.getLogger(CircuitBreakerController.class);
+    @GetMapping("/sample-api")
+    @Retry(name="default")
+    public String sampleAPI(){
+        logger.info("sample Api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8000/sample",
+                String.class);
+        return forEntity.getBody();
+    }
+}
